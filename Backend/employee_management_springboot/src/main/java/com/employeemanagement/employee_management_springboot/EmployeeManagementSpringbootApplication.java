d05c8827dfd199e14c7d30b5f7fbb66a43c0a9ca
Rushabh <rushabh99satam@gmail.com>
2022-04-21 14:04:23 +0530

    First commit

---
+++ Backend/employee_management_springboot/src/main/java/com/employeemanagement/employee_management_springboot/EmployeeManagementSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.employee_management_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringbootApplication.class, args);
+	}
+
+}
---
+++ Backend/employee_management_springboot/src/test/java/com/employeemanagement/employee_management_springboot/EmployeeManagementSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.employee_management_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
